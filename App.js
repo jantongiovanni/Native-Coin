a6ac3dcb4a89b270d3ebcdab9b5f7670764343ae
Joe Anton <janton153@gmail.com>
2019-09-09 10:10:32 -0700

    TopBar | passing props to components

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CoinView from './screens/CoinView';
+import TopBar from './components/TopBar';
 import Constants from 'expo-constants';
-
+//use constants for deveice specific content placing under the status bar / notch
 export default function App() {
   return (
     <View style={styles.container}>
         <View style={styles.statusBar} />
+        <TopBar title="Show Me The Coin" />
         <CoinView />
       </View>
   );
